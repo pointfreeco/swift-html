400836f3473a83d58b256936c790dcff30ee36c6
Brandon Williams <mbw234@gmail.com>
2019-03-26 11:37:10 -0400

    A few more tests.

--- Tests/HtmlTests/StringInterpolationTests.swift
+++ Tests/HtmlTests/StringInterpolationTests.swift
@@ -3,14 +3,30 @@ import XCTest
 
 final class StringInterpolationTests: XCTestCase {
   func testStringInterpolation() {
-    let name = "Blob<script>alert();</script>"
+    let name = "Blob"
+
+    XCTAssertEqual("""
+<div>Hello <strong>Blob</strong></div>
+""", render(Node.div("Hello \(.strong(.text(name)))")))
+
+    XCTAssertEqual("""
+<div>Hello Blob</div>
+""", render(Node.div("Hello \(name)")))
+  }
+
+  func testStringInterpolationEscaping() {
+    let xssName = "Blob<script>alert();</script>"
 
     XCTAssertEqual("""
 <div>Hello Blob&lt;script>alert();&lt;/script></div>
-""", render(Node.div("Hello \(name)")))
+""", render(Node.div("Hello \(xssName)")))
 
     XCTAssertEqual("""
 <div>Hello Blob<script>alert();</script></div>
-""", render(Node.div("Hello \(.raw(name))")))
+""", render(Node.div("Hello \(.raw(xssName))")))
+
+    XCTAssertEqual("""
+<div>Hello <strong>Blob&lt;script>alert();&lt;/script></strong></div>
+""", render(Node.div("Hello \(.strong(.text(xssName)))")))
   }
 }
